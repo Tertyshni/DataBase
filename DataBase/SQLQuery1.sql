8d64d7e30ce5decff5f5dd513c9835e53146c321
Tertyshni <a19103296@gmail.com>
2026-03-09 21:06:19 +0200

    Add files via upload

--- DataBase/SQLQuery1.sql
+++ DataBase/SQLQuery1.sql
@@ -1,79 +1,63 @@
-﻿SELECT COUNT(*) AS TeacherCount
-FROM Teachers t
-JOIN Lectures l ON t.Id = l.TeacherId
-JOIN Subjects s ON l.SubjectId = s.Id
-JOIN Departments d ON d.Id = s.Id
-WHERE d.Name = 'Software Development';
+﻿CREATE PROCEDURE GetAllDisks
+AS
+SELECT * FROM Disks;
+GO
 
-SELECT COUNT(*) AS LectureCount
-FROM Lectures l
-JOIN Teachers t ON t.Id = l.TeacherId
-WHERE t.Name = 'Dave' AND t.Surname = 'McQueen';
+CREATE PROCEDURE DisksByPublisher
+@Publisher NVARCHAR(100)
+AS
+SELECT * FROM Disks
+WHERE Publisher=@Publisher;
+GO
 
-SELECT COUNT(*) AS LectureCount
-FROM Lectures
-WHERE LectureRoom = 'D201';
+CREATE PROCEDURE MostPopularStyle
+AS
+SELECT TOP 1 Style, COUNT(*) Total
+FROM Disks
+GROUP BY Style
+ORDER BY Total DESC;
+GO
 
-SELECT LectureRoom, COUNT(*) AS LectureCount
-FROM Lectures
-GROUP BY LectureRoom;
+CREATE PROCEDURE DiskMostSongs
+@Style NVARCHAR(50)
+AS
+IF @Style='all'
+SELECT TOP 1 *
+FROM Disks
+ORDER BY SongsCount DESC
+ELSE
+SELECT TOP 1 *
+FROM Disks
+WHERE Style=@Style
+ORDER BY SongsCount DESC;
+GO
 
-SELECT COUNT(DISTINCT g.Id) AS StudentGroupsCount
-FROM Teachers t
-JOIN Lectures l ON t.Id = l.TeacherId
-JOIN GroupsLectures gl ON gl.LectureId = l.Id
-JOIN Groups g ON g.Id = gl.GroupId
-WHERE t.Name = 'Jack' AND t.Surname = 'Underhill';
+CREATE PROCEDURE DeleteDisksByStyle
+@Style NVARCHAR(50)
+AS
+BEGIN
+DELETE FROM Disks
+WHERE Style=@Style;
 
-SELECT AVG(t.Salary) AS AvgSalary
-FROM Teachers t
-JOIN Lectures l ON t.Id = l.TeacherId
-JOIN Subjects s ON s.Id = l.SubjectId
-JOIN Departments d ON d.Id = s.Id
-JOIN Faculties f ON f.Id = d.FacultyId
-WHERE f.Name = 'Computer Science';
+SELECT @@ROWCOUNT DeletedRows;
+END
+GO
 
-SELECT MIN(StudentCount) AS MinStudents,
-       MAX(StudentCount) AS MaxStudents
-FROM (
-    SELECT g.Id, COUNT(gl.GroupId) AS StudentCount
-    FROM Groups g
-    LEFT JOIN GroupsLectures gl ON g.Id = gl.GroupId
-    GROUP BY g.Id
-) AS X;
+CREATE PROCEDURE OldestAndNewestAlbum
+AS
+BEGIN
+SELECT TOP 1 * FROM Disks ORDER BY ReleaseDate;
+SELECT TOP 1 * FROM Disks ORDER BY ReleaseDate DESC;
+END
+GO
 
-SELECT AVG(Financing) AS AvgFinancing
-FROM Departments;
+CREATE PROCEDURE DeleteDisksByWord
+@Word NVARCHAR(50)
+AS
+BEGIN
+DELETE FROM Disks
+WHERE Title LIKE '%' + @Word + '%';
 
-SELECT t.Name + ' ' + t.Surname AS Teacher,
-       COUNT(DISTINCT s.Id) AS SubjectsCount
-FROM Teachers t
-JOIN Lectures l ON t.Id = l.TeacherId
-JOIN Subjects s ON l.SubjectId = s.Id
-GROUP BY t.Name, t.Surname;
-
-SELECT DayOfWeek, COUNT(*) AS LectureCount
-FROM Lectures
-GROUP BY DayOfWeek
-ORDER BY DayOfWeek;
-
-SELECT l.LectureRoom,
-       COUNT(DISTINCT d.Id) AS DepartmentsCount
-FROM Lectures l
-JOIN Subjects s ON s.Id = l.SubjectId
-JOIN Departments d ON d.Id = s.Id
-GROUP BY l.LectureRoom;
-
-SELECT f.Name AS Faculty,
-       COUNT(DISTINCT s.Id) AS SubjectsCount
-FROM Faculties f
-JOIN Departments d ON d.FacultyId = f.Id
-JOIN Subjects s ON s.Id = d.Id
-GROUP BY f.Name;
-
-SELECT t.Name + ' ' + t.Surname AS Teacher,
-       l.LectureRoom,
-       COUNT(*) AS LectureCount
-FROM Teachers t
-JOIN Lectures l ON t.Id = l.TeacherId
-GROUP BY t.Name, t.Surname, l.LectureRoom;
+SELECT @@ROWCOUNT DeletedRows;
+END
+GO

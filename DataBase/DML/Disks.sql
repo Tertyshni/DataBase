4e64a96b191113e8e17bb3f76b1f1790cbcb3475
Tertyshni <a19103296@gmail.com>
2026-03-09 21:05:29 +0200

    Add files via upload

---
+++ DataBase/DML/Disks.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO Disks (Title, Publisher, Style, SongsCount, ReleaseDate) VALUES
+('Greatest Hits', 'Sony Music', 'Rock', 15, '2001-05-10'),
+('Pop Collection', 'Universal', 'Pop', 12, '2010-03-12'),
+('Jazz Night', 'Blue Note', 'Jazz', 10, '1998-07-01'),
+('Rock Legends', 'Sony Music', 'Rock', 18, '2005-09-20'),
+('Classic Pop', 'Warner Music', 'Pop', 14, '2018-01-11'),
+('Smooth Jazz', 'Blue Note', 'Jazz', 9, '2020-06-15');
---
+++ DataBase/DML/SportShop.sql
@@ -0,0 +1,26 @@
+﻿INSERT INTO Products (Name, Type, Manufacturer, Price, Quantity) VALUES
+('Air Zoom', 'Взуття', 'Nike', 120, 10),
+('Ultraboost', 'Взуття', 'Adidas', 150, 8),
+('Football Ball', 'Мяч', 'Puma', 40, 15),
+('Sport T-shirt', 'Одяг', 'Nike', 35, 20),
+('Running Shorts', 'Одяг', 'Adidas', 30, 12);
+
+INSERT INTO Customers (Name, RegistrationDate) VALUES
+('Іван Петренко', '2020-05-12'),
+('Олег Коваль', '2019-03-15'),
+('Анна Шевченко', '2021-08-10'),
+('Марія Бондар', '2018-02-20'),
+('Дмитро Іванов', '2022-11-01');
+
+INSERT INTO Sellers (Name) VALUES
+('Сергій Коваленко'),
+('Андрій Мельник'),
+('Олександр Ткаченко');
+
+INSERT INTO Sales (ProductId, SellerId, CustomerId, Amount) VALUES
+(1,1,1,120),
+(2,1,2,150),
+(3,2,3,40),
+(4,3,4,35),
+(1,2,5,120),
+(5,3,1,30);

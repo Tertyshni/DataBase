f1df4078e3abe5a371f2d5da4522f9686d7173d3
Tertyshni <a19103296@gmail.com>
2026-03-09 21:04:54 +0200

    Add files via upload

---
+++ DataBase/DDL/Disks.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Disks
+(
+    [Id]            INT IDENTITY PRIMARY KEY,
+    [Title]         NVARCHAR(100),
+    [Publisher]     NVARCHAR(100),
+    [Style]         NVARCHAR(50),
+    [SongsCount]    INT,
+    [ReleaseDate]   DATE
+);
---
+++ DataBase/DDL/SportShop.sql
@@ -0,0 +1,33 @@
+﻿CREATE TABLE Products
+(
+    [Id]            INT IDENTITY PRIMARY KEY,
+    [Name]          NVARCHAR(100),
+    [Type]          NVARCHAR(50),
+    [Manufacturer]  NVARCHAR(100),
+    [Price]         DECIMAL(10,2),
+    [Quantity]      INT
+);
+
+CREATE TABLE Customers
+(
+    [Id]                INT IDENTITY PRIMARY KEY,
+    [Name]              NVARCHAR(100),
+    [RegistrationDate]  DATE
+);
+
+CREATE TABLE Sellers
+(
+    [Id]    INT IDENTITY PRIMARY KEY,
+    [Name]  NVARCHAR(100)
+);
+
+CREATE TABLE Sales
+(
+    [Id]            INT IDENTITY PRIMARY KEY,
+    [ProductId]     INT,
+    [SellerId]      INT,
+    [CustomerId]    INT,
+    [Amount]        DECIMAL(10,2),
+
+    
+);
